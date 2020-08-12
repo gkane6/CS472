8d196065b76df8826972065b3fd1d9ff2226f046
Guethie Kane <gkane6@student.cscc.edu>
2020-08-12 00:47:23 -0500

    W4D2

---
+++ guestListAjax/src/main/webapp/resources/js/guestts.js
@@ -0,0 +1,32 @@
+$(function () {
+  $("#send").click(updateGuests);
+});
+
+function updateGuests() {
+  var first = $("#first").val();
+  var last = $("#last").val();
+  console.log(first, last);
+
+  $.ajax("guest.ajax", {
+    type: "post",
+    data: {
+      first: first,
+      last: last,
+    },
+  }).done(displayGuests);
+}
+
+function displayGuests(data) {
+  var guestList =
+    "You need to modify this method to display the updated guest list.  Remember to build the entire list before adding it to the DOM.";
+
+  console.log(data);
+
+  $("#guestList").html(
+    `${data
+      .map((el) => {
+        return `<img src="${el}" />`;
+      })
+      .join("\n")}`
+  );
+}
